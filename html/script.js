989be68682a3c3bc9cbdccfff92b183d9060155b
victorgarrido1 <vcgarr94@gmail.com>
2023-11-26 10:53:46 -0700

    adding extra lines of code

--- html/script.js
+++ html/script.js
@@ -1,7 +1,9 @@
-
-localStorage
-
+var iconEl = document.querySelectorAll(".icon");
+var humidityEl = document.querySelectorAll(".cardHumidity");
+var tempEL = document.querySelectorAll(".cardTemp");
+var windEl = document.querySelectorAll(".cardWind");
 var pastCities = JSON.parse(localStorage.getItem("pasCities"))||[]
+
 
 const  searchBtn = document.querySelector("#searchBtn") 
 const searchCity = document.querySelector("#searchTheCity")
@@ -9,10 +11,12 @@ searchBtn.addEventListener("click", function(){
   console.log(searchCity.value)  
   searchCityBtn(searchCity.value)
 }) 
-
+// this makes function of the api
 function searchCityBtn(cityName) {
      const ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ee45939e12ce42cb8008bc94e1fd50af&units=imperial` 
      
+
+    //  fetches the api
      fetch(ApiUrl)
      .then(function (response) {
         console.log(response)
