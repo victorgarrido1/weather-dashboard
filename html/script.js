e9012eaee759c135a146b620624c010eecfbd552
victorgarrido1 <vcgarr94@gmail.com>
2023-11-26 12:59:55 -0700

    adjusting html and other parts to display info

--- html/script.js
+++ html/script.js
@@ -1,31 +1,43 @@
-var iconEl = document.querySelectorAll(".icon");
-var humidityEl = document.querySelectorAll(".cardHumidity");
-var tempEL = document.querySelectorAll(".cardTemp");
-var windEl = document.querySelectorAll(".cardWind");
-var pastCities = JSON.parse(localStorage.getItem("pasCities"))||[]
-
-
-const  searchBtn = document.querySelector("#searchBtn") 
-const searchCity = document.querySelector("#searchTheCity")
-searchBtn.addEventListener("click", function(){
-  console.log(searchCity.value)  
-  searchCityBtn(searchCity.value)
-}) 
-// this makes function of the api
+var pastCities = JSON.parse(localStorage.getItem("pastCities")) || [];
+const searchBtn = document.querySelector("#searchBtn");
+const searchCity = document.querySelector("#searchTheCity");
+searchBtn.addEventListener("click", function () {
+  console.log(searchCity.value);
+  searchCityBtn(searchCity.value);
+});
 function searchCityBtn(cityName) {
-     const ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ee45939e12ce42cb8008bc94e1fd50af&units=imperial` 
-     
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ee45939e12ce42cb8008bc94e1fd50af&units=imperial`;
+  fetch(apiUrl)
+    .then(function (response) {
+      console.log(">>>>>>>>>>>", response);
+      return response.json();
+    })
+    .then(function (data) {
+      pastCities.push(data.name);
+      localStorage.setItem("pastCities", JSON.stringify(pastCities));
+      // Update the HTML elements with the weather data
+      document.getElementById("userCity").textContent = data.name;
+      // You may need to adjust these based on the actual structure of the API response
+      document.getElementById("temp").textContent =
+        "Temp: " + data.main.temp + " °F";
+      document.getElementById("wind").textContent =
+        "Wind: " + data.wind.speed + " mph";
+      document.getElementById("humidity").textContent =
+        "Humidity: " + data.main.humidity + "%";
+      // You can continue updating other elements accordingly
+    });
+}
 
-    //  fetches the api
-     fetch(ApiUrl)
-     .then(function (response) {
-        console.log(response)
-        return response.json()
-     })
-     .then (function (data) {
-        console.log(data);
-        pastCities.push(data.name)
-        localStorage.setItem("pasCities", JSON.stringify(pastCities))
-     })
-    }
+fetch(apiURl)
 
+
+
+
+
+
+
+
+
+    
+    
+    
